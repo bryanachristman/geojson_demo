92207005cea69a788d90ab4fe32bc624a78b8097
bryanachristman <72267183+bryanachristman@users.noreply.github.com>
2020-10-02 10:03:24 -0500

    Update geojson.js

--- geojson.js
+++ geojson.js
@@ -6,9 +6,132 @@
 //
 //    See example below:
 
+
 var json = {
   "type": "FeatureCollection",
   "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.70270085334778,
+            40.81877847587765
+          ],
+          [
+            -96.70273303985596,
+            40.81918444352375
+          ],
+          [
+            -96.70129537582397,
+            40.81917632419516
+          ],
+          [
+            -96.70141339302063,
+            40.81935494919423
+          ],
+          [
+            -96.70116662979126,
+            40.81933059126812
+          ],
+          [
+            -96.70113444328308,
+            40.819168204865605
+          ],
+          [
+            -96.7002010345459,
+            40.818396864024905
+          ],
+          [
+            -96.70093059539795,
+            40.81837250574698
+          ],
+          [
+            -96.70053362846375,
+            40.81816140029714
+          ],
+          [
+            -96.69997572898865,
+            40.818104564099706
+          ],
+          [
+            -96.69826984405518,
+            40.81811268355946
+          ],
+          [
+            -96.69825911521912,
+            40.8169434711219
+          ],
+          [
+            -96.7005443572998,
+            40.81697594952343
+          ],
+          [
+            -96.70056581497192,
+            40.817260134858586
+          ],
+          [
+            -96.70079112052917,
+            40.81768235082279
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "stroke": "#a56f77",
+        "stroke-width": 2,
+        "stroke-opacity": 1
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.70259356498718,
+            40.81919256285133
+          ],
+          [
+            -96.70258283615112,
+            40.82011815968083
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "star-stroked",
+        "title": "start"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70271158218384,
+          40.81877847587765
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "large",
+        "marker-symbol": "circle-stroked",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190117124@N08/50357145076/in/album-72157716028315862/\" title=\"Photo #3: Mueller Tower\"><img src=\"https://live.staticflickr.com/65535/50357145076_982e3e68ce_n.jpg\" width=\"240\" height=\"320\" alt=\"Photo #3: Mueller Tower\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7025737836957,
+          40.82013338320493
+        ]
+      }
+    },
     {
       "type": "Feature",
       "properties": {
@@ -17,33 +140,31 @@ var json = {
         "stroke-opacity": 1,
         "fill": "#555555",
         "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190117124@N08/50357145196/in/album-72157716028315862/\" title=\"Photo #5: Andrews Hall\"><img src=\"https://live.staticflickr.com/65535/50357145196_7ca10a3229_n.jpg\" width=\"240\" height=\"320\" alt=\"Photo #5: Andrews Hall\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Polygon",
         "coordinates": [
           [
             [
-              -96.70105934143066,
-              40.806971850092815
+              -96.70228242874146,
+              40.819251427946554
             ],
             [
-              -96.69826984405518,
-              40.806971850092815
+              -96.7014268040657,
+              40.819251427946554
             ],
             [
-              -96.69826984405518,
-              40.809131953785965
+              -96.7014268040657,
+              40.819446291337385
             ],
             [
-              -96.70105934143066,
-              40.809131953785965
+              -96.70228242874146,
+              40.819446291337385
             ],
             [
-              -96.70105934143066,
-              40.806971850092815
+              -96.70228242874146,
+              40.819251427946554
             ]
           ]
         ]
@@ -52,126 +173,198 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#ee05ff",
+        "stroke": "#555555",
+        "stroke-width": 2,
+        "stroke-opacity": 1,
+        "fill": "#555555",
+        "fill-opacity": 0.5,
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190117124@N08/50356443758/in/album-72157716028315862/\" title=\"Photo #10: Closed booth\"><img src=\"https://live.staticflickr.com/65535/50356443758_f2f34ba7ce_n.jpg\" width=\"240\" height=\"320\" alt=\"Photo #10: Closed booth\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -96.70013666152954,
+              40.81716675981133
+            ],
+            [
+              -96.70012593269348,
+              40.817393092579486
+            ],
+            [
+              -96.70006692409515,
+              40.817393092579486
+            ],
+            [
+              -96.70006558299065,
+              40.81757578214306
+            ],
+            [
+              -96.69995427131653,
+              40.817577812024275
+            ],
+            [
+              -96.69994890689848,
+              40.81770569441551
+            ],
+            [
+              -96.70001059770584,
+              40.81770569441551
+            ],
+            [
+              -96.70000523328781,
+              40.817957398719344
+            ],
+            [
+              -96.6999841108918,
+              40.81795790618674
+            ],
+            [
+              -96.69998377561569,
+              40.81803859345233
+            ],
+            [
+              -96.70006424188614,
+              40.81803960838585
+            ],
+            [
+              -96.70006558299065,
+              40.818034533718034
+            ],
+            [
+              -96.70035660266876,
+              40.8180365635852
+            ],
+            [
+              -96.70035794377327,
+              40.81804468305328
+            ],
+            [
+              -96.70044243335724,
+              40.81804468305328
+            ],
+            [
+              -96.70044243335724,
+              40.81803047398351
+            ],
+            [
+              -96.70054972171783,
+              40.81803250385081
+            ],
+            [
+              -96.70054972171783,
+              40.81804468305328
+            ],
+            [
+              -96.70063287019728,
+              40.81804468305328
+            ],
+            [
+              -96.70063555240631,
+              40.818034533718034
+            ],
+            [
+              -96.70092791318893,
+              40.818037578518776
+            ],
+            [
+              -96.70092523097992,
+              40.81804214571963
+            ],
+            [
+              -96.70100435614586,
+              40.81804163825287
+            ],
+            [
+              -96.70100569725037,
+              40.81796348832775
+            ],
+            [
+              -96.70098558068275,
+              40.81796145845834
+            ],
+            [
+              -96.70098423957825,
+              40.81776430709575
+            ],
+            [
+              -96.70090176165104,
+              40.81776430709575
+            ],
+            [
+              -96.70090243220329,
+              40.81745195927158
+            ],
+            [
+              -96.70089304447174,
+              40.81745195927158
+            ],
+            [
+              -96.70089304447174,
+              40.817393092579486
+            ],
+            [
+              -96.70084744691849,
+              40.817392077636065
+            ],
+            [
+              -96.70083940029144,
+              40.817158640235796
+            ],
+            [
+              -96.70069992542267,
+              40.81716472991754
+            ],
+            [
+              -96.70069724321365,
+              40.81718502885266
+            ],
+            [
+              -96.70066505670547,
+              40.81718502885266
+            ],
+            [
+              -96.70066505670547,
+              40.81717284949233
+            ],
+            [
+              -96.7003110051155,
+              40.81717487938587
+            ],
+            [
+              -96.70030966401099,
+              40.81717792422605
+            ],
+            [
+              -96.70027479529381,
+              40.817178939172756
+            ],
+            [
+              -96.7002734541893,
+              40.81715458044764
+            ],
+            [
+              -96.70013666152954,
+              40.81716675981133
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
         "marker-size": "medium",
-        "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "marker-symbol": "park",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190117124@N08/50357306552/in/album-72157716028315862/\" title=\"Photo #6: Hammock posts\"><img src=\"https://live.staticflickr.com/65535/50357306552_a85c08d237_n.jpg\" width=\"240\" height=\"320\" alt=\"Photo #6: Hammock posts\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
-      },
-      "geometry": {
-        "type": "LineString",
-        "coordinates": [
-          [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ]
-          ]
+          -96.70070864260197,
+          40.81835728181874
         ]
       }
     }
